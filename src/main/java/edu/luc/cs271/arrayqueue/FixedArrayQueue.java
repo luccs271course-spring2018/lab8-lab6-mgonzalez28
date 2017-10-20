859ede8256064f8dc94a2c9c53351ca813c812da
Miguel Gonzalez <mgonzalez28@luc.edu>
2017-10-20 16:49:50 +0000

    Update to FindArrayQueue

--- src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
+++ src/main/java/edu/luc/cs271/arrayqueue/FixedArrayQueue.java
@@ -16,7 +16,9 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   private final E[] data;
 
-  // TODO why do we need an explicit constructor?
+  // DONE why do we need an explicit constructor?
+  // to know how much room we have in the queue based on the current size
+  //and to know where we start and where we end in the queue
 
   @SuppressWarnings("unchecked")
   public FixedArrayQueue(final int capacity) {
@@ -29,26 +31,45 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
 
   @Override
   public boolean offer(final E obj) {
-    // TODO
-    return false;
+    // DONE
+    if(size == capacity) {
+      System.out.println("Max capacity reached");
+      return false;
+    }
+    size++;
+    rear = (rear + 1) % capacity;
+    data[rear] = obj;
+    return true;
   }
 
   @Override
   public E peek() {
-    // TODO
+    // DONE
+    if (!isEmpty()) {
+      return front.data;
+    }
     return null;
   }
 
   @Override
   public E poll() {
-    // TODO
-    return null;
+    // DONE
+    if(isEmpty()) {
+      return null;
+    }
+    E result = data[front];
+    front = (front + 1) % capacity;
+    size--;
+    return result;
   }
 
   @Override
   public boolean isEmpty() {
-    // TODO
-    return true;
+    // DONE
+    if (size == 0) {
+      return true;
+    }
+    return false;
   }
 
   @Override
@@ -59,6 +80,11 @@ public class FixedArrayQueue<E> implements SimpleQueue<E> {
   @Override
   public List<E> asList() {
     // TODO implement using an ArrayList preallocated with the right size
+    while(front != null) {
+      Queue<E> arrQueue= new ArrayList<E>(capacity);
+      list.add(front.data);
+      front = front.next;
+    }
     return Arrays.asList();
   }
 }
